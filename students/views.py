35eb78d2983d2176215af2c0314c502fded39e33
Orest Mykha <orestmykha.53@gail.com>
2015-05-12 17:05:19 +0300

    add students app to ur project

--- students/views.py
+++ students/views.py
@@ -1,10 +1,31 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
+#Views for Students
+
 def students_list(request):
-	return render(request, 'students/students_list.html', {})
+	students = (
+		{'id': 1,
+		 'first_name': u'Корост',
+		 'last_name': u'Андрій',
+		 'ticket': 2123,
+		 'image': 'img/pin.png'},
+		{'id': 2,
+		 'first_name': u'Корпюк',
+		 'last_name': u'Роман',
+		 'ticket': 215,
+		 'image': 'img/pin.png'},
+		{'id': 3,
+		 'first_name': u'Гриців',
+		 'last_name': u'Віктор',
+		 'ticket': 28,
+		 'image': 'img/pin.png'},
+	)
+	return render(request, 'students/students_list.html', {'students': students})
 	
 def students_add(request):
 	return HttpResponse('<h1>Student Add Form</h1>')
