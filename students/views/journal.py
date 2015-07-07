ee6c717dc5186bbe3f3fd014be6d2c08902498d9
Orest Mykha <orestmykha.53@gail.com>
2015-07-07 10:18:30 +0300

    chapter revision

--- students/views/journal.py
+++ students/views/journal.py
@@ -4,6 +4,7 @@ from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
 from calendar import monthrange, weekday, day_abbr
 
+from django.http import JsonResponse
 from django.core.urlresolvers import reverse
 from django.views.generic.base import TemplateView
 
@@ -27,8 +28,8 @@ class JournalView(TemplateView):
 		
 		# calculate current, previous and next month detalis;
 		# we need this for month navigation element in template_name
-		next_month = month + relativedelta(month=1)
-		prev_month = month - relativedelta(month=1)
+		next_month = month + relativedelta(months=1)
+		prev_month = month - relativedelta(months=1)
 		context['prev_month'] = prev_month.strftime('%Y-%m-%d')
 		context['next_month'] = next_month.strftime('%Y-%m-%d')
 		context['year'] = month.year
@@ -46,7 +47,7 @@ class JournalView(TemplateView):
 			for d in range(1, number_of_days+1)]
 			
 		# get all students from database
-		queryset = Student.objects.order_by('last_name')
+		queryset = Student.objects.all().order_by('last_name')
 		
 		# url to update student presence, for from post
 		update_url = reverse('journal')
@@ -64,24 +65,42 @@ class JournalView(TemplateView):
 				
 			# fill in days presence list for current student
 			days = []
-			for day in range(1, 31):
+			for day in range(1, number_of_days+1):
 				days.append({
 					'day': day,
-					'present': True,
-					'date': date(2014, 7, day).strftime(
-						'%Y-%m-%d'),
+					'present': journal and getattr(journal, 'present_day%d' % day, False) or False,
+					'date': date(myear, mmonth, day).strftime('%Y-%m-%d'),
 				})
 			
-		# prepare metadata for current student
-		students.append({
-			'fullname': u'%s %s' % (student.last_name, student.first_name),
-			'days': days,
-			'id': student.id,
-			'update_url': update_url,
-		})
+			# prepare metadata for current student
+			students.append({
+				'fullname':u'%s %s' % (student.last_name, student.first_name),
+				'days':days,
+				'id':student.id,
+				'update_url':update_url,
+			})
 		
 		# apply pagination, 10 students per page
 		context = paginate(students, 10, self.request, context, var_name='students')
 		
 		# finaly return updated context with paginated students
 		return context
+		
+	def post(self, request, *args, **kwargs):
+		data = request.POST
+		
+		# prepare student, dates and presence data
+		current_date = datetime.strptime(data['date'], '%Y-%m-%d').date()
+		month = date(current_date.year, current_date.month, 1)
+		present = data['present'] and True or False
+		student = Student.objects.get(pk=data['pk'])
+		
+		# get or create journal object for given student and month
+		journal = MonthJournal.objects.get_or_create(student=student, date=month)[0]
+		
+		# set new presence on journal for given student and save result
+		setattr(journal, 'present_day%d' % current_date.day, present)
+		journal.save()
+		
+		# return success status
+		return JsonResponse({'status': 'success'})
