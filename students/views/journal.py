523c818cd3731452a56e68fbe4fba497b62f24b6
Orest Mykha <orestmykha.53@gail.com>
2015-07-07 14:49:13 +0300

    chapter 9 add new logic views

--- students/views/journal.py
+++ students/views/journal.py
@@ -46,8 +46,12 @@ class JournalView(TemplateView):
 			'verbose': day_abbr[weekday(myear, mmonth, d)][:2]}
 			for d in range(1, number_of_days+1)]
 			
-		# get all students from database
-		queryset = Student.objects.all().order_by('last_name')
+		# get all students from database, or just one if we need to
+		# display journal for one student
+		if kwargs.get('pk'):
+			queryset = [Student.objects.get(pk=kwargs['pk'])]
+		else:
+			queryset = Student.objects.all().order_by('last_name')
 		
 		# url to update student presence, for from post
 		update_url = reverse('journal')
