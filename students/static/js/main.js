e542b29ccc1d1ea4591a4200d16d66470e71a71a
Orest Mykha <orestmykha.53@gail.com>
2015-07-07 21:16:12 +0300

    chapter 9 add new function

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -27,6 +27,30 @@ function initJournal() {
 	});
 }
 
+function initGroupSelector() {
+	// look up select element with groups and our even handler
+	// on field "change" event
+	$('#group-selector select').change(function(event) {
+		//get value of currently selected group option
+		var group = $(this).val();
+		
+		if(group) {
+			//set cookie with expiration date 1 years since now;
+			// cookie creation function takes period in days
+			$.cookie('current_group', group, {'path': '/', 'expires': 365});
+		} else {
+			// otherwise we delete the cookie
+		$.removeCookie('current_group', {'path': '/'});	
+		}
+		
+		//and reload a pageX
+		location.reload(true);
+		
+		return true;
+	});
+}
+
 $(document).ready(function() {
 	initJournal();
+	initGroupSelector();
 });
