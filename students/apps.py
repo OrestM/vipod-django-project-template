7f990daf3545cce7d53930c8a04203be2ab77be4
Orest Mykha <orestmykha.53@gail.com>
2015-07-29 12:45:37 +0300

    fixed some bugs

--- students/apps.py
+++ students/apps.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 class StudentAppConfig(AppConfig):
 	name = 'students'
-	verbose_name = u'База Студентів'
+	verbose_name = _(u'Base Student')
 	
 	def ready(self):
 		from students import signals
