7485a907bd5c338ce7fddb8df8a26914faf656a2
Orest Mykha <orestmykha.53@gail.com>
2015-08-17 21:46:26 +0300

    chapter 12

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -35,16 +35,16 @@ js_info_dict = {
 urlpatterns = patterns('',
 	# Students urls
 	url(r'^$', 'students.views.students.students_list', name='home'),
-	url(r'^students/add/$', StudentCreateView.as_view(), name='students_add'),
-	url(r'^students/(?P<pk>\d+)/edit/$', StudentUpdateView.as_view(), name='students_edit'),
-	url(r'^students/(?P<pk>\d+)/delete/$', StudentDeleteView.as_view(), name='students_delete'),
+	url(r'^students/add/$', login_required(StudentCreateView.as_view()), name='students_add'),
+	url(r'^students/(?P<pk>\d+)/edit/$', login_required(StudentUpdateView.as_view()), name='students_edit'),
+	url(r'^students/(?P<pk>\d+)/delete/$', login_required(StudentDeleteView.as_view()), name='students_delete'),
 	
 	
 	#Groups urls
 	url(r'^groups/$', 'students.views.groups.groups_list', name='groups'),
-	url(r'^groups/add/$', GroupAddView.as_view(), name='groups_add'),
-	url(r'^groups/(?P<pk>\d+)/edit/$', GroupUpdateView.as_view(), name='groups_edit'),
-	url(r'^groups/(?P<pk>\d+)/delete/$', GroupDeleteView.as_view(), name='groups_delete'),
+	url(r'^groups/add/$', login_required(GroupAddView.as_view()), name='groups_add'),
+	url(r'^groups/(?P<pk>\d+)/edit/$', login_required(GroupUpdateView.as_view()), name='groups_edit'),
+	url(r'^groups/(?P<pk>\d+)/delete/$', login_required(GroupDeleteView.as_view()), name='groups_delete'),
 	
 	#Journal
 	url(r'^journal/(?P<pk>\d+)?/?$', JournalView.as_view(), name='journal'),
@@ -52,9 +52,9 @@ urlpatterns = patterns('',
 	
 	#Exam
 	url(r'^exam/$', 'students.views.exam.exam_list', name='exam'),
-	url(r'^exam/add/$', ExamCreateView.as_view(), name='exam_add'),
-	url(r'^exam/(?P<pk>\d+)/edit/$', ExamUpdateView.as_view(), name='exam_edit'),
-	url(r'^exam/(?P<pk>\d+)/delete/$', ExamDeleteView.as_view(), name='exam_delete'),
+	url(r'^exam/add/$', login_required(ExamCreateView.as_view()), name='exam_add'),
+	url(r'^exam/(?P<pk>\d+)/edit/$', login_required(ExamUpdateView.as_view()), name='exam_edit'),
+	url(r'^exam/(?P<pk>\d+)/delete/$', login_required(ExamDeleteView.as_view()), name='exam_delete'),
 		
 	# Contact Admin Form
 	url(r'^contact-admin/$', 'students.views.contact_admin.contact_admin', name='contact_admin'),
